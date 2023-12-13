9740faceef146cd491437b7e63e084518e54df56
seonekr <oudonephengkhamlar@gmail.com>
2023-12-13 17:17:28 +0700

    Update Products2.jsx

--- src/user/components/products/Products2.jsx
+++ src/user/components/products/Products2.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import "./products.css";
-import Header from "../header/Header";
-import Menu from "../menu/Menu";
-import { FaSearch } from "react-icons/fa";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import DataTable from "react-data-table-component";
@@ -13,61 +9,109 @@ const columns = [
   {
     name: "ID",
     selector: (row) => row.id,
+    sortable: true,
   },
   {
     name: "Name",
     selector: (row) => row.name,
+    sortable: true,
+  },
+  {
+    name: "Price",
+    selector: (row) => row.price,
+    sortable: true,
+  },
+  {
+    name: "Color",
+    selector: (row) => row.colors,
+  },
+  {
+    name: "Cover",
+    selector: (row) => row.image,
+	cell: row => <img src={row.image} width={100} alt={row.name}></img>,
+	width: '150'
   },
 ];
 
 const Products = () => {
   const [data, setData] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [totalRows, setTotalRows] = useState(0);
-	const [perPage, setPerPage] = useState(10);
+  const [loading, setLoading] = useState(false);
+  const [totalRows, setTotalRows] = useState(0);
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(10);
+  const [sortColumn, setSortColumn] = useState("");
+  const [sortColumnDir, setSortColumnDir] = useState("");
+  const [search, setSearch] = useState("");
 
-	const fetchData = async page => {
-		setLoading(true);
+  const fetchData = async () => {
+    setLoading(true);
 
-		const response = await axios.get(`http://localhost:5000/searchProducts?page=${page}&per_page=${perPage}&delay=1`);
+    var url = `http://localhost:5000/searchProducts?page=${page}&per_page=${perPage}`;
 
-		setData(response.data.data);
-		setTotalRows(response.data.total);
-		setLoading(false);
-	};
+	if(search) {
+		url += `&search=${search}`;
+	}
+    if (sortColumn) {
+      url += `&sortColumn=${sortColumn}&sortColumnDir=${sortColumnDir}`;
+    }
 
-	const handlePageChange = page => {
-		fetchData(page);
-	};
+    const response = await axios.get(url);
 
-	const handlePerRowsChange = async (newPerPage, page) => {
-		setLoading(true);
+    setData(response.data.data);
+    setTotalRows(response.data.total);
+    setLoading(false);
+  };
 
-		const response = await axios.get(`http://localhost:5000/searchProducts?page=${page}&per_page=${newPerPage}&delay=1`);
+  const handlePageChange = (page) => {
+    setPage(page);
+  };
 
-		setData(response.data.data);
-		setPerPage(newPerPage);
-		setLoading(false);
-	};
+  const handlePerRowsChange = async (newPerPage, page) => {
+    setPerPage(newPerPage);
+  };
 
-	useEffect(() => {
-		fetchData(1); // fetch page 1 of users
-		
-	}, []);
+  const handleSort = (column, sortDirection) => {
+    console.log(column);
+    setSortColumn(column.name);
+    setSortColumnDir(sortDirection);
+  };
 
-	return (
-		<DataTable
-			title="Users"
-			columns={columns}
-			data={data}
-			progressPending={loading}
-			pagination
-			paginationServer
-			paginationTotalRows={totalRows}
-			onChangeRowsPerPage={handlePerRowsChange}
-			onChangePage={handlePageChange}
-		/>
-	);
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    fetchData();
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [page, perPage, sortColumn, sortColumnDir]);
+
+  return (
+    <div>
+      <form onSubmit={handleSearchSubmit}>
+        <label>
+          Search:
+          <input type="text" name="search" onChange={handleSearchChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <DataTable
+        title="Products"
+        columns={columns}
+        data={data}
+        progressPending={loading}
+        pagination
+        paginationServer
+        paginationTotalRows={totalRows}
+        onChangeRowsPerPage={handlePerRowsChange}
+        onChangePage={handlePageChange}
+        onSort={handleSort}
+      />
+    </div>
+  );
 };
 
 export default Products;
